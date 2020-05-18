6bd37601eb2f2e783d180fb2ea2a045f6b25eeba
mihaicat10 <mihai.ionescu1094@gmail.com>
2020-05-18 16:40:46 +0300

    Updates

---
+++ precache-manifest.0ba0ad0c43e8b315cee8fcff22b3e7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4caa4ffd2b2b07703bdee7aa185e224",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bc81c911adb93f98fd9e",
+    "url": "/monsters-rolodex/static/css/main.451b46a7.chunk.css"
+  },
+  {
+    "revision": "909e779f7f39663f06e4",
+    "url": "/monsters-rolodex/static/js/2.8a45e223.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8a45e223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc81c911adb93f98fd9e",
+    "url": "/monsters-rolodex/static/js/main.fa97dfeb.chunk.js"
+  },
+  {
+    "revision": "bb407335468ce50c2344",
+    "url": "/monsters-rolodex/static/js/runtime-main.6027e332.js"
+  }
+]);
